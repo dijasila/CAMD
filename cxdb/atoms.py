20623e19ed095d26b0b842681cfc656a30621cdf
Jens Jørgen Mortensen <jj@smoerhul.dk>
2023-11-29 12:34:34 +0100

    Improved atoms plot

--- cxdb/atoms.py
+++ cxdb/atoms.py
@@ -1,10 +1,14 @@
 import json
-
+from functools import cache
 import plotly
 import plotly.graph_objects as go
+import numpy as np
 
 from cxdb.section import Section
 from cxdb.material import Material
+from ase.data import covalent_radii
+from ase.data.colors import jmol_colors
+from ase.neighborlist import neighbor_list
 
 HTML = """
 <h4>{formula}</h4>
@@ -38,7 +42,55 @@ class AtomsSection(Section):
     def plot(self, material: Material, repeat: int = 1) -> str:
         assert repeat < 5, 'DOS!'
         atoms = material.atoms * repeat
-        x, y, z = atoms.positions.T
-        fig = go.Figure(data=[go.Scatter3d(x=x, y=y, z=z,
-                                           mode='markers')])
+        fig = plot_atoms(atoms)
         return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+
+def plot_atoms(atoms):
+    data = []
+    v, t = sphere()
+    i, j, k = t.T
+    for Z, xyz in zip(atoms.numbers, atoms.positions):
+        x, y, z = (v * covalent_radii[Z] * 0.5 + xyz).T
+        a, b, c = (jmol_colors[Z] * 255).astype(int)
+        color = f'rgb({a},{b},{c})'
+        mesh = go.Mesh3d(x=x, y=y, z=z,
+                         i=i, j=j, k=k, opacity=1,
+                         color=color)
+        data.append(mesh)
+    p = atoms.positions
+    i, j, D, S = neighbor_list('ijDS', atoms,
+                               cutoff=covalent_radii[atoms.numbers] * 1.2)
+    xyz = np.empty((3, len(i) * 3))
+    xyz[:, 0::3] = p[i].T
+    D[S.any(1)] *= 0.5
+    xyz[:, 1::3] = (p[i] + D).T
+    xyz[:, 2::3] = np.nan
+    x, y, z = xyz
+    data.append(go.Scatter3d(x=x,
+                             y=y,
+                             z=z,
+                             mode='lines')
+                )
+    fig = go.Figure(data=data)
+    fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
+    fig.update_xaxes(showgrid=False)
+    fig.update_layout(template='simple_white')
+    fig.update_scenes(aspectmode='data')
+    return fig
+
+
+@cache
+def sphere():
+    from gpaw.sphere.lebedev import R_nv as points
+    from scipy.spatial import ConvexHull
+    hull = ConvexHull(points)
+    return points, hull.simplices
+
+
+if __name__ == '__main__':
+    import sys
+    from ase.io import read
+    atoms = read(sys.argv[1])
+    fig = plot_atoms(atoms)
+    fig.show()
