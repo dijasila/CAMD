8953f0709ba85ca36caa5cba1a17ca8e66af603c
Jens Jørgen Mortensen <jjmo@dtu.dk>
2024-01-12 06:53:58 +0100

    Add CLI test

---
+++ cxdb/test/cli_test.py
@@ -0,0 +1,14 @@
+from ase import Atoms
+
+from cxdb.cli import main
+
+
+def test_cli(tmp_path):
+    f = tmp_path / 'H2'
+    f.mkdir()
+    atoms = Atoms('H2', [(0, 0, 0), (0.7, 0, 0)], pbc=True)
+    atoms.center(vacuum=2)
+    h2 = f / 'h2.xyz'
+    atoms.write(h2)
+    app = main([str(h2)])
+    assert 'Volume' in app.index()
