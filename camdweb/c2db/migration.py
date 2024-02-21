d0d4d94018cee3598a354f67bbc8c44126ee46ca
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2024-02-21 17:32:47 +0100

    ...

--- camdweb/c2db/migration.py
+++ camdweb/c2db/migration.py
@@ -4,13 +4,13 @@ from ase.formula import Formula
 from camdweb.c2db.copy import PATTERNS, read_result_file
 
 fro = Path('/home/niflheim/joaso/paper_calcs/htp-spiral_paper/n1-magnets/tree')
-to = Path('/home/niflheim2/cmr/C2DB-ASR/tree')
+to = Path('/home/niflheim2/cmr/C2DB-ASR/')
 
 
 def copy_spinspiral_data_to_tree():  # pragma: no cover
     dirs = [dir
             for pattern in PATTERNS
-            for dir in fro.glob(pattern)
+            for dir in to.glob(pattern)
             if dir.name[0] != '.']
     paths = {}
     for dir in dirs:
@@ -22,11 +22,15 @@ def copy_spinspiral_data_to_tree():  # pragma: no cover
             continue
         f = Formula(olduid.split('-')[0])
         stoi, reduced, nunits = f.stoichiometry()
-        path = to / f'{stoi}/{f}/{olduid}'
+        path = fro / f'{stoi}/{f}/{olduid}'
         paths[path] = dir
 
+    print(len(paths))
     for path in fro.glob('A*/*/*/'):
-        dir = paths[path]
+        if path not in paths:
+            print(path)
+            continue
+        dir = paths.pop(path)
         for name in ['collect_spiral',
                      'dmi',
                      'spinorbit',
@@ -35,7 +39,12 @@ def copy_spinspiral_data_to_tree():  # pragma: no cover
             if f1.is_file():
                 f2 = dir / f1.name
                 assert not f2.is_file()
-                if 0:
+                if 1:
                     f2.write_bytes(f1.read_bytes())
                 else:
                     print(f1, f2)
+    print(len(paths))
+
+
+if __name__ == '__main__':
+    copy_spinspiral_data_to_tree()
