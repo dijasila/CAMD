267c3127ef28f8bb3fd0f3d16c5918098ab9ce6c
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2024-02-21 16:58:57 +0100

    ...

--- camdweb/c2db/migration.py
+++ camdweb/c2db/migration.py
@@ -1,20 +1,39 @@
 from pathlib import Path
 
+from ase.formula import Formula
+from camdweb.c2db.copy import PATTERNS, read_result_file
+
 fro = Path('/home/niflheim/joaso/paper_calcs/htp-spiral_paper/n1-magnets/tree')
 to = Path('/home/niflheim2/cmr/C2DB-ASR/tree')
 
 
-def copy_spinspiral_data_to_tree():
-    for p1 in fro.glob('A*/*/*/'):
-        p2 = to.joinpath(p1.parts[-3:])
-        assert p2.is_dir()
+def copy_spinspiral_data_to_tree():  # pragma: no cover
+    dirs = [dir
+            for pattern in PATTERNS
+            for dir in fro.glob(pattern)
+            if dir.name[0] != '.']
+    paths = {}
+    for dir in dirs:
+        fp = dir / 'results-asr.database.material_fingerprint.json'
+        try:
+            olduid = read_result_file(fp)['uid']
+        except FileNotFoundError:
+            print('No fingerprint:', fp)
+            continue
+        f = Formula(olduid.split('-')[0])
+        stoi, reduced, nunits = f.stoichiometry()
+        path = to / f'{stoi}/{f}/{olduid}'
+        paths[path] = dir
+
+    for path in fro.glob('A*/*/*/'):
+        dir = paths[path]
         for name in ['collect_spiral',
                      'dmi',
                      'spinorbit',
                      'spinorbit@calculate']:
-            f1 = p1 / f'results-asr.{name}.json'
+            f1 = path / f'results-asr.{name}.json'
             if f1.is_file():
-                f2 = p2 / f1.name
+                f2 = dir / f1.name
                 assert not f2.is_file()
                 if 0:
                     f2.write_bytes(f1.read_bytes())
