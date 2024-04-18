5b266d0a806f40f91197b230b33c7220bb03072b
Kartikeya Sharma <kartsh@dtu.dk>
2024-04-18 12:41:46 +0200

    Fix minor mypy and changed a variable name

--- camdweb/qpod/copy_files.py
+++ camdweb/qpod/copy_files.py
@@ -36,10 +36,8 @@ RESULT_FILES = [
     'gs',
     'gs@calculate']
 
-CHARGE_0_RESULT_FILES = ['defectinfo',
+charge_0_result_files = ['defectinfo',
                          'sj_analyze']
-RESULT_FILES.extend(CHARGE_0_RESULT_FILES)
-
 
 ROOT = Path('/home/niflheim2/cmr/WIP/defects/rerun-qpod')
 
@@ -76,23 +74,26 @@ def copy_material(dir: Path, names: defaultdict[str, int]) -> None:
     # Handle pristine and defect folders
     material_folder = parts[defects_index - 1].split('-')[0]
 
+    # Avoiding modifying global variable
+    result_files = RESULT_FILES.copy()
+
     if 'defects.pristine' in str(dir):
         subfolder = parts[defects_index].split('.')[-1]
         folder = Path(material_folder) / subfolder
-        RESULT_FILES.extend(['defectinfo'])
-        RESULT_FILES.extend(['charge_neutrality'])
+        result_files.extend(['defectinfo'])
+        result_files.extend(['charge_neutrality'])
     else:
         defects_parts = parts[defects_index].split('.')
         subfolder = defects_parts[-1]
         charge_folder = parts[defects_index + 1]
         folder = Path(material_folder) / subfolder / charge_folder
-        RESULT_FILES.extend(['database.material_fingerprint'])
+        result_files.extend(['database.material_fingerprint'])
         
         if charge_folder == 'charge_0':
-            RESULT_FILES.extend(CHARGE_0_RESULT_FILES)
+            result_files.extend(charge_0_result_files)
         
         if (folder / 'results-asr.defect_symmetry.json').is_file():
-            RESULT_FILES.extend(['defect_symmetry'])
+            result_files.extend(['defect_symmetry'])
 
     # def rrf(name: str) -> dict:
     #     return read_result_file(dir / f'results-asr.{name}.json')
@@ -117,8 +118,8 @@ def copy_material(dir: Path, names: defaultdict[str, int]) -> None:
         data['host_gap_hse'] = defectinfo['host_gap_hse']
         data['host_hof'] = defectinfo['host_hof']
         data['host_uid'] = defectinfo['host_uid']
-        data['host_spacegroup'] = defectinfo['host_spacegroup']
-        data['host_pointgroup'] = defectinfo['host_pointgroup']
+        data['host_spacegroup'] = str(defectinfo['host_spacegroup']) or ''
+        data['host_pointgroup'] = str(defectinfo['host_pointgroup']) or ''
         # Defect-defect distance; 0 inplace of None:
         data['r_nn'] = defectinfo['R_nn'] or 0.0
     except FileNotFoundError:
@@ -146,7 +147,7 @@ def copy_material(dir: Path, names: defaultdict[str, int]) -> None:
     atoms.write(folder / 'structure.xyz')
 
     # Copy result json-files:
-    for name in RESULT_FILES:
+    for name in result_files:
         result = dir / f'results-asr.{name}.json'
 
         if 'defects.pristine' in str(dir) and name == 'magstate':
@@ -155,11 +156,14 @@ def copy_material(dir: Path, names: defaultdict[str, int]) -> None:
         if result.is_file():
             shutil.copyfile(result, folder / result.name)
 
+    # Remove None values:
+    data = {key: value for key, value in data.items() if value is not None}
+
     (folder / 'data.json').write_text(json.dumps(data, indent=0))
 
 ##### MAJOR HACKY FIX FOR *known* CURSED JSON FILES #####
 ##### This can be removed once the json files are fixed ##### 
-def fix_json_file(filepath: str):
+def fix_json_file(filepath: str | Path):
     with open(filepath, 'r') as file:
         lines = file.readlines()
 
@@ -184,7 +188,7 @@ def fix_json_file(filepath: str):
             with open(filepath, 'w') as file:
                 file.writelines(lines[:-1])
 
-def is_json_file_well_formatted(filepath: str) -> bool:
+def is_json_file_well_formatted(filepath: str | Path) -> bool:
     try:
         with open(filepath, 'r') as file:
             json.load(file)
